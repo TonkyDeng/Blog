9c624ef83848d69470c38541a3d12ec1e13690b8
Tonky.Deng <tonkydeng@hotmail.com>
2017-11-05 16:55:34 +0800

    add Donate and Contact

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,15 @@
 $(function(){
-	$(".index li").click(function(){
+	$(".wrap .index").on("click","li",function(){
 		$(".selected").removeClass('selected');
 		$(this).addClass('selected');
 	});
+
 });
+
+function getDonate(){
+	var str="<div id='lbody'><div id='byte-info'><div class='close-info'>×</div><img id='byte' src='https://od.lk/s/NjRfODkzOTQ1NF8/donate_me.png'/></div>";
+	$("body").after(str);	
+	$(".close-info").click(function(){
+		$("#lbody").remove();
+	});
+}
